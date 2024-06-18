550feee78c7f274a355ce538d9c1b90e9952605d
PardhivA <pardhiv19nov@gmail.com>
2024-06-18 16:57:35 +0530

    Update a listing done

--- api/controllers/listing_controller.js
+++ api/controllers/listing_controller.js
@@ -27,3 +27,22 @@ export const deleteListingController = async (req,res,next) => {
         next(error)
     }
 }
+
+export const updateListingController = async (req,res,next) => {
+    const listing=  await Listing.findById(req.params.id)
+    if(!listing){
+        return next(errorHandler(404, "Listing not found"))
+    }
+    if(req.user.id != listing.userRef.toString()) return next(errorHandler(401, "you are not allowed to update this listing"))
+    try{
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, 
+           req.body
+            , {new : true}  )
+            // const {password, ...rest} = updatedUser._doc
+
+            res.status(200).json(updatedListing)
+    }
+    catch(error){
+        next(error)
+    }
+}
--- api/routes/listing_route.js
+++ api/routes/listing_route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {createListing, deleteListingController} from '../controllers/listing_controller.js'
+import {createListing, deleteListingController, updateListingController} from '../controllers/listing_controller.js'
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = Router()
 
 router.post('/create',verifyToken, createListing);
 router.delete('/delete/:id',verifyToken, deleteListingController)
+router.post('/update/:id',verifyToken, updateListingController)
 
 export default router
