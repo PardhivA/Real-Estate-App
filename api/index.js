a5314318dfdd8566ab1c1b2b169d65df6141c099
PardhivA <pardhiv19nov@gmail.com>
2024-06-23 00:46:58 +0530

    added pre installation config

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ import user_route from './routes/user_route.js'
 import auth_route from './routes/auth_route.js'
 import listing_route from './routes/listing_route.js'
 import cookieParser from "cookie-parser"
-
+import path from 'path'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB ');
 }).catch((err) => {console.log(err)})
+
+const __dirname = path.resolve();
+
 
 const app = express();
 
@@ -22,16 +25,23 @@ app.listen(3000, () => {
 });
 
 // app.get('/', (req, res) => {
-//     res.send("Hello World")
-// })
+    //     res.send("Hello World")
+    // })
+    
 
-app.use('/api/user', user_route)
+
+    app.use('/api/user', user_route)
 app.use('/api/auth', auth_route)
 app.use('/api/listing', listing_route)
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next ) => {
-        const statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || 500;
         const message = err.message || 'Internal Server Error';
         return res.status(statusCode).json({
             success : false,
